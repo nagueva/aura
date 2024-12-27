c8a50053b9be4472f79fdfe643ada83dca14c86c
G. Nagüeva <guilherme@nagueva.com>
2024-12-27 21:19:34 +0100

    fix: theme compiler

--- tools/theme_compiler.py
+++ tools/theme_compiler.py
@@ -7,6 +7,9 @@ ROOT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../')
 
 # Function to clean a folder
 def clean_folder(folder_path):
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+        print(f"Created folder: {folder_path}")
     print(f"Cleaning folder...")
     for filename in os.listdir(folder_path):
         file_path = os.path.join(folder_path, filename)
